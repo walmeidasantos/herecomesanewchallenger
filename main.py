5f02d1589e26cabc820f1d84dc575146e8474e5e
walmeidasantos <walmeidasanto@gmail.com>
2024-04-14 19:01:02 -0300

    testes com sqlmodel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from sqlmodel import Field, SQLModel, create_engine
 from typing import Optional
+from sqlmodel import Field, SQLModel, create_engine, Session, select
 import os 
 from dotenv import dotenv_values
 import psycopg2
@@ -8,18 +8,37 @@ import psycopg2
 config = dotenv_values(".env")
 
 DATABASE_URI = "postgresql://" + config['POSTGRES_USER'] + ":" + config['POSTGRES_PASSWORD'] + "@"+ config['POSTGRES_HOST'] + ":5438/" + config["POSTGRES_DB"]
-vector = create_engine(DATABASE_URI,echo=True)
-SQLModel.metadata.create_all(vector)
-
 
 class Pessoa(SQLModel, table=True):
 
     id: Optional[int] = Field(default=None,primary_key=True)
-    name: str
+    name: str = Field(max_length=100)
+    nickname: str = Field(max_length=30)
 
 
 if __name__ == "__main__":
-     print(DATABASE_URI)
-       
+    #  SQLModel.metadata.create_all(vector)
+    vector = create_engine(DATABASE_URI,echo=True)
+    eumesmo = Pessoa(name="Teste pessoa 1")
+    aMinhaNamoradatambem = Pessoa(name="Teste pessoa 2")
+    session = Session(vector)
+
+    # session.add(eumesmo)
+    # session.add(aMinhaNamoradatambem)
+    # session.commit()
+    results = session.exec( select(Pessoa).where(Pessoa.id==1) )
+    # print(eumesmo.name)
+    # print(aMinhaNamoradatambem.name)
+    # for pessoa in results:
+    #     print(pessoa)
+        
+    pessoa = results.first()
+    print(pessoa)   
+    pessoa.name="Wellingtao"
+    session.add(pessoa)
+    session.commit()
+    session.refresh(pessoa)
+    print(pessoa)
+    session.close
 
 
