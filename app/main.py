c5690584d9fd34a9ee8040287c1f1a500116c966
walmeidasantos <walmeidasanto@gmail.com>
2024-05-04 12:58:15 -0300

    Colocando tratamento assíncrono nos métodos.(realmente nos testes fazem muita diferença) Corrigindo o nome da classe módulo, assim como os campos para adequar ao teste da rinha. Corrigindo o tratamento de excessões

--- app/main.py
+++ app/main.py
@@ -2,18 +2,12 @@ from fastapi import FastAPI, HTTPException, Query, Response, Request, Depends
 import uvicorn
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
 from sqlmodel import Session, text, select
 from db import init_db, get_session
 from models import Pessoa, PessoaJson
 
-
 app = FastAPI(title="Rinha Backend 2023")
-
-
 # app.debug = True
-def serialize(data) -> str:
-    return json.dumps(data, default=str)
 
 
 @app.on_event("startup")
@@ -22,27 +16,16 @@ def on_startup():
 
 
 @app.exception_handler(RequestValidationError)
-async def validation_exception_handler(req: Request, exc: RequestValidationError):
-    details = exc.errors()
-    if any(
-        detail["type"] == "string_type" and isinstance(detail["input"], int)
-        for detail in details
-    ):
-        return JSONResponse(
-            status_code=422,
-            content=jsonable_encoder({"detail": details}),
-        )
-    return JSONResponse(
-        status_code=422,
-        content=jsonable_encoder({"detail": details}),
-    )
+async def validation_exception_handler(request, handler):
+    return Response(status_code=400)
 
 
-@app.post("/pessoas", status_code=201, response_model_exclude_unset=True)
-def create(res: Response, pessoa: PessoaJson, session: Session = Depends(get_session)):
-
+@app.post("/pessoas", status_code=201)
+async def create_person(
+    person: PessoaJson, res: Response, session: Session = Depends(get_session)
+):
     try:
-        new_pessoa = Pessoa.model_validate(pessoa)
+        new_pessoa = Pessoa.model_validate(person)
         pessoa_existe = session.exec(
             select(Pessoa).where(Pessoa.apelido == new_pessoa.apelido)
         ).first()
@@ -52,35 +35,35 @@ def create(res: Response, pessoa: PessoaJson, session: Session = Depends(get_ses
         session.add(new_pessoa)
         session.commit()
         session.refresh(new_pessoa)
-        # res.headers.update({"Location": f"/pessoas/{new_pessoa.id}"})
+        res.headers["Location"] = f"/pessoas/{new_pessoa.id}"
         return new_pessoa
     except ValueError:
         raise HTTPException(status_code=400)
-    return serialize(new_pessoa)
 
 
 @app.get("/pessoas/{id}")
-def find_by_id(uuid: str, session: Session = Depends(get_session)):
+async def find_by_id(id: str, session: Session = Depends(get_session)):
 
     try:
-        pessoa_existe = session.exec(select(Pessoa).where(Pessoa.id == uuid)).first()
+        pessoa_existe = session.exec(select(Pessoa).where(Pessoa.id == id)).first()
         if pessoa_existe:
             return pessoa_existe
-        raise HTTPException(status_code=404)
+        else:
+            raise HTTPException(status_code=404)
     except:
         raise HTTPException(status_code=400)
 
 
 @app.get("/pessoas", response_model=list[Pessoa])
-def find_by_term(
-    termo: str = Query(..., min_length=1), session: Session = Depends(get_session)
+async def find_by_term(
+    term: str = Query(..., alias="t"), session: Session = Depends(get_session)
 ):
     pessoas = []
     try:
-        raw_query = "SELECT id, nome,apelido,stack,nascimento FROM pessoa WHERE search_text ilike :termo "
-        result = session.execute(
-            text(raw_query), {"termo": "%" + termo + "%"}
-        ).fetchmany(30)
+        raw_query = "SELECT id, nome,apelido,stack,nascimento FROM pessoa WHERE search_text ilike :term"
+        result = session.execute(text(raw_query), {"term": "%" + term + "%"}).fetchmany(
+            30
+        )
 
         for row in result:
             pessoa = Pessoa(
@@ -90,7 +73,7 @@ def find_by_term(
                 stack=row.stack,
                 nascimento=row.nascimento,
             )
-            pessoas.append(pessoa.dict())
+            pessoas.append(pessoa.model_dump())
 
         return pessoas
     except:
@@ -102,5 +85,5 @@ def count_pessoas(session: Session = Depends(get_session)):
     return session.query(Pessoa).count()
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8003)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=9999)
